0c84179ad87145af5cf2df77e4d0564c960361da
erivelton <erivelton.bg@hotmail.com>
2024-08-15 15:57:22 -0300

    update in cron

--- src/shared/infra/http/nodeCron/index.ts
+++ src/shared/infra/http/nodeCron/index.ts
@@ -1,10 +1,17 @@
 import cron from 'node-cron';
 import axios from 'axios';
 
-cron.schedule(' * * * * *', async() => {
+cron.schedule('* * * * *', async() => {
   console.log('running a task every minute');
   console.log('running: http://localhost:3333/billing');
   await axios.post(`http://localhost:3333/billing`);
+}, {
+  scheduled: true,
+  timezone: "America/Sao_Paulo"
+});
+
+cron.schedule('* * * * *', async() => {
+  console.log('running a task every minute');
   console.log('running: http://localhost:3333/billing/send/email');
   await axios.post(`http://localhost:3333/billing/send/email`);
 }, {
